9433ca639d772cb7c9e1e7b714792a415c291b59
theboyulove <phuhieupham4@gmail.com>
2023-05-05 17:43:30 +0700

    change folder

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -3,41 +3,44 @@ const cheerio = require('cheerio');
 const prettier = require('prettier');
 
 exports.handler = async (event, context) => {
-  const articleUrl = 'https://aubtu.biz/100680/'; // replace with your article URL
+  const articleUrl = 'https://selectednews.live/former-nfl-participant-colin-allred-raises-file-2-million-in-first-day-of-senate-marketing-campaign-sd-news';
+
   const response = await fetch(articleUrl);
   const html = await response.text();
 
   const $ = cheerio.load(html);
 
-  // Extract the title and content of the article
-  const title = $('h1.entry-title').text().trim();
-  const content = $('.entry-content').html();
+  // Get the title of the article
+  const title = $('h1.entry-title').text();
 
-  // Extract the images in the article and replace the "data-src" attribute with "src"
-  $('img[data-src]').each(function () {
-    $(this).attr('src', $(this).attr('data-src'));
-  });
+  // Get the main content of the article
+  const articleContent = $('div.entry-content').html();
 
-  // Prettify the HTML output
-  const prettyHtml = prettier.format(content, { parser: 'html' });
+  // Get the featured image of the article
+  const featuredImageUrl = $('div.entry-content img').first().attr('src');
 
-  // Combine the title and content into a single HTML document
-  const outputHtml = `
-    <html>
-      <head>
-        <title>${title}</title>
-      </head>
-      <body>
-        ${prettyHtml}
-      </body>
-    </html>
-  `;
+  // Format the HTML output using Prettier
+  const formattedHtml = prettier.format(
+    `
+      <html>
+        <head>
+          <title>${title}</title>
+        </head>
+        <body>
+          <h1>${title}</h1>
+          <img src="${featuredImageUrl}">
+          ${articleContent}
+        </body>
+      </html>
+    `,
+    { parser: 'html' }
+  );
 
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'text/html',
     },
-    body: outputHtml,
+    body: formattedHtml,
   };
 };
