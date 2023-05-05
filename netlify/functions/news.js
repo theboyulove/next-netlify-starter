68dbb6fac775f206df99daac7b93277d45dc3415
theboyulove <phuhieupham4@gmail.com>
2023-05-05 17:40:37 +0700

    add cheerio

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -3,44 +3,41 @@ const cheerio = require('cheerio');
 const prettier = require('prettier');
 
 exports.handler = async (event, context) => {
-  const articleUrl = 'https://aubtu.biz/100680/';
-
+  const articleUrl = 'https://aubtu.biz/100680/'; // replace with your article URL
   const response = await fetch(articleUrl);
   const html = await response.text();
 
   const $ = cheerio.load(html);
 
-  // Get the title of the article
-  const title = $('h1.entry-title').text();
+  // Extract the title and content of the article
+  const title = $('h1.entry-title').text().trim();
+  const content = $('.entry-content').html();
 
-  // Get the main content of the article
-  const articleContent = $('div.entry-content').html();
+  // Extract the images in the article and replace the "data-src" attribute with "src"
+  $('img[data-src]').each(function () {
+    $(this).attr('src', $(this).attr('data-src'));
+  });
 
-  // Get the featured image of the article
-  const featuredImageUrl = $('div.entry-content img').first().attr('src');
+  // Prettify the HTML output
+  const prettyHtml = prettier.format(content, { parser: 'html' });
 
-  // Format the HTML output using Prettier
-  const formattedHtml = prettier.format(
-    `
-      <html>
-        <head>
-          <title>${title}</title>
-        </head>
-        <body>
-          <h1>${title}</h1>
-          <img src="${featuredImageUrl}">
-          ${articleContent}
-        </body>
-      </html>
-    `,
-    { parser: 'html' }
-  );
+  // Combine the title and content into a single HTML document
+  const outputHtml = `
+    <html>
+      <head>
+        <title>${title}</title>
+      </head>
+      <body>
+        ${prettyHtml}
+      </body>
+    </html>
+  `;
 
   return {
     statusCode: 200,
     headers: {
       'Content-Type': 'text/html',
     },
-    body: formattedHtml,
+    body: outputHtml,
   };
 };
