d46e4b1d67b8f27c63ffeb2eb002a174d1ad77c8
theboyulove <phuhieupham4@gmail.com>
2023-05-05 18:44:20 +0700

    add cheerio

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -6,46 +6,42 @@ exports.handler = async (event, context) => {
   const { id } = event.queryStringParameters;
   const articleUrl = `https://criticsbreakingnews.co.uk/?p=${id}`;
 
-  try {
-    const response = await fetch(articleUrl);
-    const html = await response.text();
+  const response = await fetch(articleUrl);
+  const html = await response.text();
 
-    const $ = cheerio.load(html);
+  const $ = cheerio.load(html);
 
-    // Get the title of the article
-    const title = $('h1.post-title').text();
+  // Get the title of the article
+  const title = $('h1.post-title').text();
 
-    // Get the main content of the article
-    const articleContent = $('div.post-content').html();
+  // Get the main content of the article
+  const articleContent = $('div.post-content').html();
 
-    // Get the featured image of the article
-    const featuredImageUrl = $('div.post-image img').attr('src');
+  // Get the featured image of the article
+  const featuredImageUrl = $('div.post-image img').first().attr('src');
 
-    // Format the HTML output using Prettier
-    const formattedHtml = prettier.format(
-      `
-        <html>
-          <head>
-            <title>${title}</title>
-          </head>
-          <body>
-            <h1>${title}</h1>
-            <img src="${featuredImageUrl}">
-            ${articleContent}
-          </body>
-        </html>
-      `,
-      { parser: 'html' }
-    );
+  // Format the HTML output using Prettier
+  const formattedHtml = prettier.format(
+    `
+      <html>
+        <head>
+          <title>${title}</title>
+        </head>
+        <body>
+          <h1>${title}</h1>
+          <img src="${featuredImageUrl}">
+          ${articleContent}
+        </body>
+      </html>
+    `,
+    { parser: 'html' }
+  );
 
-    return {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'text/html',
-      },
-      body: formattedHtml,
-    };
-  } catch (err) {
-    return { statusCode: 500, body: err.toString() };
-  }
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: formattedHtml,
+  };
 };
