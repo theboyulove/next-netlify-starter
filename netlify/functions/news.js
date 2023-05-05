3e5c737838b1add16575baeb8c454215dd8d13d8
theboyulove <phuhieupham4@gmail.com>
2023-05-05 18:35:33 +0700

    add cheerio

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -4,36 +4,46 @@ const prettier = require('prettier');
 
 exports.handler = async (event, context) => {
   const { id } = event.queryStringParameters;
-  const url = `https://aubtu.biz/${id}`;
+  const articleUrl = `https://aubtu.biz/${id}`;
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(articleUrl);
     const html = await response.text();
+
     const $ = cheerio.load(html);
 
-    const title = $('h1.title').text();
-    const content = $('div.content').html();
-    const image = $('img').attr('src');
-    const articleHTML = `
-      <html>
-        <head>
-          <title>${title}</title>
-        </head>
-        <body>
-          <h1>${title}</h1>
-          <img src="${image}">
-          ${content}
-        </body>
-      </html>
-    `;
+    // Get the title of the article
+    const title = $('h1.post-title').text();
 
-    const formattedHTML = prettier.format(articleHTML, { parser: 'html' });
+    // Get the main content of the article
+    const articleContent = $('div.post-content').html();
+
+    // Get the featured image of the article
+    const featuredImageUrl = $('div.post-image img').attr('src');
+
+    // Format the HTML output using Prettier
+    const formattedHtml = prettier.format(
+      `
+        <html>
+          <head>
+            <title>${title}</title>
+          </head>
+          <body>
+            <h1>${title}</h1>
+            <img src="${featuredImageUrl}">
+            ${articleContent}
+          </body>
+        </html>
+      `,
+      { parser: 'html' }
+    );
+
     return {
       statusCode: 200,
       headers: {
-        'Content-type': 'text/html',
+        'Content-Type': 'text/html',
       },
-      body: formattedHTML,
+      body: formattedHtml,
     };
   } catch (err) {
     return { statusCode: 500, body: err.toString() };
