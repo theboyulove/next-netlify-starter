5a3db01f40130f4bed8cb815ff73061816174f94
theboyulove <phuhieupham4@gmail.com>
2023-05-05 17:28:00 +0700

    Updated code

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -1,40 +1,17 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
-const prettier = require('prettier');
 
 exports.handler = async (event, context) => {
   // Fetch the article from the website
-  const articleUrl = 'https://aubtu.biz/100680/';
+  const articleUrl = 'https://example.com/article';
   const response = await fetch(articleUrl);
   const html = await response.text();
 
   // Extract the article content
   const $ = cheerio.load(html);
-  const articleTitle = $('h1.entry-title').text().trim();
+  const articleTitle = $('h1.entry-title').text();
   const articleContent = $('div.entry-content').html();
-  const articleImages = $('div.entry-content img');
-
-  // Update image src to absolute URLs
-  articleImages.each((index, element) => {
-    const originalSrc = $(element).attr('src');
-    const absoluteSrc = new URL(originalSrc, articleUrl).toString();
-    $(element).attr('src', absoluteSrc);
-  });
-
-  // Format the HTML content using Prettier
-  const formattedHtml = prettier.format(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset="UTF-8">
-        <title>${articleTitle}</title>
-      </head>
-      <body>
-        <h1>${articleTitle}</h1>
-        ${articleContent}
-      </body>
-    </html>
-  `, { parser: 'html' });
+  const articleImage = $('div.entry-content img').attr('data-src');
 
   // Return the article content as the response
   return {
@@ -42,6 +19,17 @@ exports.handler = async (event, context) => {
     headers: {
       'Content-Type': 'text/html',
     },
-    body: formattedHtml,
+    body: `
+      <html>
+        <head>
+          <title>${articleTitle}</title>
+        </head>
+        <body>
+          <h1>${articleTitle}</h1>
+          <img src="${articleImage}" loading="lazy" />
+          ${articleContent}
+        </body>
+      </html>
+    `,
   };
 };
