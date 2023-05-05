1ee6bb4c67ba20d7af11d3f46473d96013dffabf
theboyulove <phuhieupham4@gmail.com>
2023-05-05 17:09:09 +0700

    add prettier

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -3,34 +3,32 @@ const cheerio = require('cheerio');
 const prettier = require('prettier');
 
 exports.handler = async (event, context) => {
-  // Fetch the article from the website
-  const articleUrl = 'https://aubtu.biz/100680/';
-  const response = await fetch(articleUrl);
-  const html = await response.text();
+  try {
+    // Fetch the article from the website
+    const articleUrl = 'https://aubtu.biz/100680/';
+    const response = await fetch(articleUrl);
+    const html = await response.text();
 
-  // Extract the article content
-  const $ = cheerio.load(html);
-  const articleContent = $('.article-content').html();
-  const articleImages = $('.article-content img')
-    .map((_, element) => $(element).attr('src'))
-    .get();
+    // Extract the article content
+    const $ = cheerio.load(html);
+    const articleContent = $('.article-content').html();
 
-  // Format the article content using Prettier
-  const formattedContent = prettier.format(articleContent, {
-    parser: 'html',
-    printWidth: 80,
-    htmlWhitespaceSensitivity: 'ignore',
-  });
+    // Format the article content with Prettier
+    const formattedContent = articleContent ? prettier.format(articleContent, { parser: 'html' }) : '';
 
-  // Return the article content and images as the response
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html',
-    },
-    body: JSON.stringify({
-      content: formattedContent,
-      images: articleImages,
-    }),
-  };
+    // Return the article content as the response
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'text/html',
+      },
+      body: formattedContent,
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
 };
