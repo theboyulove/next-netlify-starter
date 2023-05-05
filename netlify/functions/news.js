7800e933f14f87a68c1257bf0d1dc4f0de9bd7ae
theboyulove <phuhieupham4@gmail.com>
2023-05-05 20:53:55 +0700

    add cheerio

--- netlify/functions/news.js
+++ netlify/functions/news.js
@@ -1,24 +1,24 @@
+const fetch = require('node-fetch');
+const { parse } = require('node-html-parser');
 const prettier = require('prettier');
 
 exports.handler = async (event, context) => {
   const articleId = event.queryStringParameters.id;
   const articleUrl = `https://criticsbreakingnews.co.uk/?p=${articleId}`;
 
-  const fetch = await import('node-fetch');
-  const response = await fetch.default(articleUrl);
+  const response = await fetch(articleUrl);
   const html = await response.text();
-
-  const cheerio = await import('cheerio');
-  const $ = cheerio.default.load(html);
+  const root = parse(html);
 
   // Get the title of the article
-  const title = $('h1.entry-title').text();
+  const title = root.querySelector('meta[property="og:title"]').getAttribute('content');
 
   // Get the main content of the article
-  const articleContent = $('div.entry-content').html();
+  const contentElements = root.querySelectorAll('.entry-content p');
+  const content = contentElements.map(el => el.toString()).join('\n');
 
   // Get the featured image of the article
-  const featuredImageUrl = $('div.entry-content img').first().attr('src');
+  const featuredImageUrl = root.querySelector('meta[property="og:image"]').getAttribute('content');
 
   // Format the HTML output using Prettier
   const formattedHtml = prettier.format(
@@ -30,7 +30,7 @@ exports.handler = async (event, context) => {
         <body>
           <h1>${title}</h1>
           <img src="${featuredImageUrl}">
-          <div>${articleContent}</div>
+          ${content}
         </body>
       </html>
     `,
